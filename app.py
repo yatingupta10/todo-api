9516593a5dc191852c223ef11f0e6540aafd987b
yatingupta10 <yatingupta12@gmail.com>
2016-06-22 11:15:20 +0530

    added delete task method

--- app.py
+++ app.py
@@ -68,5 +68,13 @@ def update_task(task_id):
     task[0]['done'] = request.json.get('done', task[0]['done'])
     return jsonify({'task': task[0]})
 
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'result': True})
+
 if __name__ == '__main__':
     app.run(debug=True)
