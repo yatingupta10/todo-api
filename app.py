cf0662627446e35a89c98c8be20a8e8a102cd24d
yatingupta10 <yatingupta12@gmail.com>
2016-06-22 11:10:04 +0530

    added post method

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask, jsonify
 from flask import abort
 from flask import make_response
+from flask import request
 
 app = Flask(__name__)
 
@@ -35,6 +36,19 @@ def get_task(task_id):
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
